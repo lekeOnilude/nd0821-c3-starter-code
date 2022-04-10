8379615905111477f0707aaacb20ecf20f4387c5
Gbemileke <gbemileke@datasciencenigeria.ai>
2022-04-10 16:19:45 +0100

    Set up Heroku

--- starter/main.py
+++ starter/main.py
@@ -1,9 +1,16 @@
 # Put the code for your API here.
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+import os
 
 import pandas as pd
 import joblib
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
 
